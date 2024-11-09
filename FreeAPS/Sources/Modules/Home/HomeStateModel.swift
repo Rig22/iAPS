e6622c7840c359084ef6948dcf13981c1526b724
Richard <chuma@live.de>
2024-11-09 02:32:03 +0100

    backgroundColors update

--- FreeAPS/Sources/Modules/Home/HomeStateModel.swift
+++ FreeAPS/Sources/Modules/Home/HomeStateModel.swift
@@ -121,28 +121,8 @@ extension Home {
 
         var selectedBackgroundColor: Color {
             switch BackgroundColorOption(rawValue: backgroundColorOptionRawValue) {
-            case .darkBlue:
-                return Color(red: 0.08, green: 0.15, blue: 0.20)
-            case .blue:
-                return Color(red: 0.10, green: 0.20, blue: 0.50)
-            case .teal:
-                return Color(red: 0.00, green: 0.36, blue: 0.36)
-            case .darkGreen:
-                return Color(red: 0.00, green: 0.39, blue: 0.00)
-            case .black:
-                return Color(red: 0.00, green: 0.00, blue: 0.00)
-            case .gray:
-                return Color(red: 0.12, green: 0.14, blue: 0.14)
-            case .blackBerry:
-                return Color(red: 0.23, green: 0.04, blue: 0.14)
-            case .red:
-                return Color(red: 0.4, green: 0.0, blue: 0.0)
-            case .burntOrange:
-                return Color(red: 0.45, green: 0.22, blue: 0.12)
-            case .purple:
-                return Color(red: 0.36, green: 0.20, blue: 0.72)
             default:
-                return .clear // Standardfarbe, falls keine Übereinstimmung
+                return .clear // Standardfarbe, falls keine Übereinstimmung gefunden wird
             }
         }
 
--- FreeAPS/Sources/Modules/Home/View/BackgroundColorOption.swift
+++ FreeAPS/Sources/Modules/Home/View/BackgroundColorOption.swift
@@ -8,11 +8,12 @@ enum BackgroundColorOption: String, CaseIterable, Identifiable, Encodable {
     case teal
     case darkGreen
     case black
-    case gray
+    case darkGray
     case blackBerry
-    case red
+    case darkRed
     case burntOrange
-    case purple
+    case mustard
+    case aubergine
 
     var id: String { rawValue }
 
@@ -25,19 +26,21 @@ enum BackgroundColorOption: String, CaseIterable, Identifiable, Encodable {
         case .teal:
             return Color(red: 0.00, green: 0.36, blue: 0.36)
         case .darkGreen:
-            return Color(red: 0.00, green: 0.39, blue: 0.00)
+            return Color(red: 0.10, green: 0.25, blue: 0.15)
         case .black:
             return Color(red: 0.00, green: 0.00, blue: 0.00)
-        case .gray:
+        case .darkGray:
             return Color(red: 0.12, green: 0.14, blue: 0.14)
         case .blackBerry:
             return Color(red: 0.23, green: 0.04, blue: 0.14)
-        case .red:
-            return Color(red: 0.4, green: 0.0, blue: 0.0)
+        case .darkRed:
+            return Color(red: 0.25, green: 0.07, blue: 0.10)
         case .burntOrange:
             return Color(red: 0.45, green: 0.22, blue: 0.12)
-        case .purple:
-            return Color(red: 0.36, green: 0.20, blue: 0.72)
+        case .mustard:
+            return Color(red: 0.28, green: 0.22, blue: 0.10)
+        case .aubergine:
+            return Color(red: 0.15, green: 0.05, blue: 0.25)
         }
     }
 }
