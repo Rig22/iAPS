57855ed5e6724de0fe1c84908ac1a40a96305517
Richard <chuma@live.de>
2025-01-28 15:09:22 +0100

    show most frequent loop error in statistic

--- FreeAPS/Sources/Models/Statistics.swift
+++ FreeAPS/Sources/Models/Statistics.swift
@@ -1,3 +1,4 @@
+
 import Foundation
 
 struct Statistics: JSON, Equatable {
@@ -103,6 +104,8 @@ extension Statistics {
 struct LoopCycles: JSON, Equatable {
     var loops: Int
     var errors: Int
+    var mostFrequentErrorType: String
+    var mostFrequentErrorAmount: Int
     var readings: Int
     var success_rate: Decimal
     var avg_interval: Decimal
@@ -172,6 +175,8 @@ extension LoopCycles {
     private enum CodingKeys: String, CodingKey {
         case loops
         case errors
+        case mostFrequentErrorType
+        case mostFrequentErrorAmount
         case readings
         case success_rate
         case avg_interval
